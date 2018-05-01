255b1e963bc2fa43400e127429d9a0d42bd058b8
Deu desu ~~ <deuSan@Esraas-MacBook-Pro.local>
2018-05-01 18:28:23 +0200

    startPage&login

---
+++ mashro3k/external/customizedViews/UIView+Custom.h
@@ -0,0 +1,17 @@
+//
+//  UIView+Custom.h
+//  ZakatFund
+//
+//  Created by Ahmed Osama on 1/29/14.
+//  Copyright (c) 2014 asgatech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Custom)
+
+@property (nonatomic) CGFloat x;
+@property (nonatomic) CGFloat y;
+//@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@end
